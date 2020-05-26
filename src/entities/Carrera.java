65d885dbb56797a6725ef65ef9eba7ea63984029
juanjose <juanjosecg@ufps.edu.co>
2020-05-26 12:11:15 -0500

    Subiendo El Proyecto Modelo Simple

---
+++ src/entities/Carrera.java
@@ -0,0 +1,61 @@
+package entities;
+
+
+public class Carrera {
+	
+	private String codigo;
+	private String nombre;
+	private Integer creditos;
+	private Integer semestres;
+	
+	
+	public Carrera() {
+		super();
+	}
+
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public Integer getCreditos() {
+		return creditos;
+	}
+
+
+	public void setCreditos(Integer creditos) {
+		this.creditos = creditos;
+	}
+
+
+	public Integer getSemestres() {
+		return semestres;
+	}
+
+
+	public void setSemestres(Integer semestres) {
+		this.semestres = semestres;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/pruebas/Pruebas.java
@@ -0,0 +1,37 @@
+package pruebas;
+
+import entities.Carrera;
+import servicios.CarreraDao;
+
+public class Pruebas {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Carrera carrera = new Carrera();
+		CarreraDao cDao = new CarreraDao();
+		
+		carrera.setCodigo("120");
+		carrera.setNombre("Ingeniería de Sistemas");
+		carrera.setSemestres(10);
+		carrera.setCreditos(160);
+		
+		System.out.println(cDao.listar().size());
+		
+	//	cDao.insertar(carrera);
+		
+		Carrera c2 = cDao.buscar("120");
+		if(c2!=null){
+			//System.out.println(c2.getNombre());
+		//	c2.setNombre("Ing Computacional");
+		//	cDao.actualizar(c2);
+		//	c2 = cDao.buscar("120");
+		//	System.out.println(c2.getNombre());
+			cDao.eliminar(carrera);
+	}	
+
+	}
+
+}
